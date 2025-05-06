6531dbfb021dd35be9d832bca32e38ba05bb44fb
easin <easin562050@gmail.com>
2025-05-06 23:02:16 +0600

    operators part done

---
+++ arithmetic_operations.py
@@ -0,0 +1,10 @@
+a=10
+b=20
+
+print(a+b) # This is the addition operation
+print(a-b) # This is the subtraction operation
+print(a*b) # This is the multiplication operation
+print(a/b) # This is the divition operation
+print(a//b) # This is the floor division. This remove the decimal value
+print(a%b) # This is the modulus operation
+print(a**b) # This is the exponent opetion. This will act like a power
---
+++ identity_operations.py
@@ -0,0 +1,11 @@
+''' It is used to compare the memory location of two elements
+is - True is the same memory location
+is not - True is the memory location is not same
+'''
+
+a=[1,2,3]
+b=a
+c=[1,2,3]
+
+print(a is b)
+print(a is c)
