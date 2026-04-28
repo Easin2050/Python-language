d3a994843df7180165b94b07f48fb92d711bfb03
easin <easin562050@gmail.com>
2026-04-28 23:25:25 +0600

    variables practice done

---
+++ data_types.py
@@ -0,0 +1,12 @@
+def sumation(a,b):
+    return int(a+b)
+
+user=int(input("Give a number:"))
+
+def main():
+    input1=int(input("Give the first number:"))
+    input2=int(input("Give the second number:"))
+    if user==1:
+        print(f'The sum of this two number:{sumation(input1,input2)}')
+
+main()
---
+++ variables.py
@@ -0,0 +1,16 @@
+first_name=input('Enter your first name:')
+second_name=input('Enter your second name:')
+CGPA=float(input("Enter your CGPA:"))
+skills=['C','C++','Python','DSA']
+personal_info={
+    'address':'Dhaka',
+    'age':'40',
+    'Married':True,
+    'Country': 'Bangladesh'
+
+}
+print("First name:",first_name)
+print("Second name:",second_name)
+print("CGPA:",CGPA)
+print("Skills:",skills)
+print("Personal:",personal_info)
