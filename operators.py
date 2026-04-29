437120ae2d5f947e3456ad7b43c47909d3bc2283
easin <easin562050@gmail.com>
2026-04-29 23:12:09 +0600

    operators and exercise done

---
+++ operators.py
@@ -0,0 +1,57 @@
+import cmath
+'''
+#1
+age=int(input("Enter your age:"))
+#2
+height=float(input("Enter your height:"))
+#3
+complex_number=(3+4j)
+#4
+number=complex(input("Enter a complex number:"))
+#5 Area of a triangle
+base=int(input("Enter the base:"))
+height=int(input("Enter the height:"))
+print(f"The area of the triangle is {int(0.5*base*height)}")
+#6 Perimeter of triangle
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+perimeter = a + b + b + c  
+semi_perimeter = (a + b + c) / 2
+
+print(f"The perimeter of the triangle is {perimeter:.2f}")
+print(f"The semi-perimeter of the triangle is {semi_perimeter:.2f}")
+
+#7 Area of a triangle
+lenght=int(input("Enter the length:"))
+width=int(input("Enter the width:"))
+
+print(f"The area of the triangle is {lenght*width}")
+print(f"The perimeter of the triangle is {2*(lenght+width)}")
+
+#8 Radius of the circle
+r=float(input("Enter the radius of the circle:"))
+print(f"The radius of the circle is {3.14*r**2}")
+
+#9 Slope of the line
+x=float(input("Enter the x coordinate of the first point:"))
+y=(2*x)-2
+print(f"The y coordinate of the first point is {y}")
+
+x1=int(input("Enter x1:"))
+x2=int(input("Enter x2:"))
+y1=int(input("Enter y1:"))
+y2=int(input("Enter y2:"))
+
+slope=(y2-y1)/(x2-x1)
+print(f"The slope of the line is {slope}")
+
+#10 Compare of the slopes in tasks 8 and 9
+if y<slope:
+    print("The slope of the line in task 8 is less than the slope of the line in task 9.")
+elif(slope>y):
+    print("The slope of the line in task 8 is greater thn the slope of the line in task 9.")
+else:
+    print("The slope of the line in task 8 is equal to the slope of the line in task 9.")'''
+
